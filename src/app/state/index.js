e8f68137ea115a7dd128b3200c4e400a7bf43589
Evaldas <evaldas.cerniauskas@gmail.com>
2019-03-25 21:51:53 +0200

    add webpack and merge config

--- src/app/state/index.js
+++ src/app/state/index.js
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 import middlewares from "./middlewares";
 
-const store = createStore(reducers, applyMiddleware(...middlewares));
+const appliedMiddlewares = applyMiddleware(...middlewares);
+const finalMiddlewares =
+  process.env.NODE_ENV === "productions"
+    ? appliedMiddlewares
+    : composeWithDevTools(appliedMiddlewares);
+const store = createStore(reducers, finalMiddlewares);
 
 export default store;
